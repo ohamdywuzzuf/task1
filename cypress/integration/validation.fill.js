09bdeadfaa4d32d72fbf27a1b48553ce8994e872
ohamdywuzzuf <101634147+ohamdywuzzuf@users.noreply.github.com>
2022-04-14 16:01:25 +0200

    Update validation.fill.js

--- cypress/integration/validation.fill.js
+++ cypress/integration/validation.fill.js
@@ -5,7 +5,7 @@ describe('Sign up form validate', function() {
 //    it('Click on each field for empty input', function() { --- need help with 'it' function
         cy.get('input[name=firstname]').type('Omar')
         cy.get('input[name=lastname]').type('Hamdy')
-        cy.get('input[name=email]').type('omar@hamdyz.com')
+        cy.get('input[name=email]').type('omar@hamdy.net')
         cy.get('input[name=password]').type('omarhamdy')
         cy.get('button[type=submit].css-6lejne').click()
         cy.wait(5000)
